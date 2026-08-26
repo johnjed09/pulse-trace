2b8ebe769341aa4249ec2fea42f8e1345798ad14
Jed Dev <getesjohnjed09@gmail.com>
2026-08-26 22:03:45 +0800

    Added UI url for allowed backend access

--- main.py
+++ main.py
@@ -20,7 +20,12 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
-origins = ["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:5173"]
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:5173",
+    "https://johnjed09.github.io",
+]
 
 app.add_middleware(
     CORSMiddleware,
